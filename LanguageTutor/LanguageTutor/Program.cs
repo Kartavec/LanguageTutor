f790bf4a69d218f92ba56cc7cb97be2d205a35f3
kartavec <kartavec@gmail.com>
2020-01-25 19:10:51 +0300

    Дорабатываем консольную версию

--- LanguageTutor/LanguageTutor/Program.cs
+++ LanguageTutor/LanguageTutor/Program.cs
@@ -11,12 +11,21 @@ namespace LanguageTutor
             tutor.AddWord("hello", "привет");
             tutor.AddWord("dog", "собака");
             tutor.AddWord("cat", "кошка");
-            tutor.AddWord("byke", "велосипед");
 
-            if(tutor.CheckWord("dog", "Cобака"))
+            while (true)
             {
-
+                var word = tutor.GetRandomEngWord();
+                Console.WriteLine($"Как переводится слово: {word}?");
+                var userAnswer = Console.ReadLine();
+                if (tutor.CheckWord(word, userAnswer))
+                    Console.WriteLine("Правильно!");
+                else
+                {
+                    var correctAnswer = tutor.Translate(word);
+                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+                }
             }
+
         }
     }
 }
