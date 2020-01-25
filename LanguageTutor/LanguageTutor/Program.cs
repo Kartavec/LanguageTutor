6e6094d891db7c73c875107afa184c36a5ac29a4
kartavec <kartavec@gmail.com>
2020-01-25 18:34:35 +0300

    Используем Dictionary и HashSet

--- LanguageTutor/LanguageTutor/Program.cs
+++ LanguageTutor/LanguageTutor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LanguageTutor
 {
@@ -6,7 +7,35 @@ namespace LanguageTutor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1);
+            set.Add(2);
+            set.Add(1);
+
+            if (set.Contains(1))
+            {
+                Console.WriteLine("В hashset-е хранится элемент 1");
+            }
+
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("dog", "собака");
+            dic.Add("cat", "кошка");
+
+
+            Console.WriteLine($"dog переводится как {dic["dog"]}");
+            Console.WriteLine($"В словаре у нас {dic.Count} значений");
+
+            foreach (var key in dic.Keys)
+            {
+                Console.WriteLine($"{key} переводится как {dic[key]}");
+            }
+
+            if (dic.ContainsKey("dog"))
+            {
+
+
+            }
         }
     }
 }
