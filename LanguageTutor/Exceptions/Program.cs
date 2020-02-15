ec65c87fed1aa385f8b87b814c1ba5a10f84c24d
kartavec <kartavec@gmail.com>
2020-02-15 17:15:24 +0300

    Обработка исключений при чтении файла

---
+++ LanguageTutor/Exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = GetRowCount("C:/Path/file.txt");
+            Console.WriteLine(r);
+        }
+
+        static int GetRowCount(string path)
+        {
+            try
+            {
+                var rows = File.ReadAllLines(path);
+                return rows.Length;
+            }
+            catch (FileNotFoundException ex)
+            {
+                //                Console.WriteLine($"Файл {path} не был найден! Алярм!");
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Функция была вызвана с пустым аргументом. Так делать нехорошо!");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Не получилось считать файл");
+                return 0;
+            }
+        }
+    }
+}
