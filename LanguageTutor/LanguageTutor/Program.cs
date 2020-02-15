8a8a099b93137d9e70f7690fe07ea68cf12004e0
kartavec <kartavec@gmail.com>
2020-02-15 19:12:26 +0300

    Создан новый телеграм-бот

--- LanguageTutor/LanguageTutor/Program.cs
+++ LanguageTutor/LanguageTutor/Program.cs
@@ -1,27 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using Telegram.Bot;
 
 namespace LanguageTutor
 {
     class Program
     {
+
+        static TelegramBotClient Bot;
         static void Main(string[] args)
         {
-            var tutor = new Tutor();
+            Bot = new TelegramBotClient("1077974262:AAFLGbCcG10-1TWHyNALbjwF3JhyJBjOrKY");
 
-            while (true)
-            {
-                var word = tutor.GetRandomEngWord();
-                Console.WriteLine($"Как переводится слово: {word}?");
-                var userAnswer = Console.ReadLine();
-                if (tutor.CheckWord(word, userAnswer))
-                    Console.WriteLine("Правильно!");
-                else
-                {
-                    var correctAnswer = tutor.Translate(word);
-                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
-                }
-            }
+            //var me = Bot.GetMeAsync().Result;
+            //Console.WriteLine(me.FirstName);
+
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+
+
+
+            //var tutor = new Tutor();
+            //tutor.AddWord("rabbit", "кролик");
+            //while (true)
+            //{
+            //    var word = tutor.GetRandomEngWord();
+            //    Console.WriteLine($"Как переводится слово: {word}?");
+            //    var userAnswer = Console.ReadLine();
+            //    if (tutor.CheckWord(word, userAnswer))
+            //        Console.WriteLine("Правильно!");
+            //    else
+            //    {
+            //        var correctAnswer = tutor.Translate(word);
+            //        Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+            //    }
+            //}
+
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+            Console.WriteLine(e.Message.From.Username);
 
         }
     }
